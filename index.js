89a4a921522f5f61dfdfcbcc6f361657f4966a92
Ismael <258471924+ismaelsoaresdev@users.noreply.github.com>
2026-09-08 21:22:01 -0300

    implement express

--- index.js
+++ index.js
@@ -1,39 +1,31 @@
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
 const http = require('node:http');
 const fs = require('node:fs/promises');
 const path = require('node:path');
 
-const server = http.createServer(async (req, res) => {
-  console.log('URL requisitada:', req.url);
-  let filePath;
+app.use(express.json());
 
-  switch (req.url) {
-    case '/':
-    case '/index':
-      filePath = path.join(__dirname, 'index.html');
-      break;
-    case '/about':
-      filePath = path.join(__dirname, 'about.html');
-      break;
-    case '/contact-me':
-      filePath = path.join(__dirname, 'contact-me.html');
-      break;
-    default:
-      filePath = path.join(__dirname, '404.html');
-      break;
-  }
-
-  try {
-    const data = await fs.readFile(filePath, 'utf8');
-    const statusCode = req.url === '/404' || (req.url !== '/' && req.url !== '/index' && req.url !== '/about' && req.url !== '/contact-me') ? 404 : 200;
-    
-    res.writeHead(statusCode, { 'Content-Type': 'text/html' });
-    res.end(data);
-  } catch (err) {
-    res.writeHead(500, { 'Content-Type': 'text/plain' });
-    res.end('Erro interno no servidor');
-  }
+app.listen(PORT, () => {
+  console.log(`Server runnig on port:${PORT}`);
 });
 
-server.listen(8080, () => {
-  console.log('Servidor rodando em http://localhost:8080');
+  app.get('/', (req, res) => {
+  res.sendFile('/', { root: __dirname });
 });
+
+     app.get('/about', (req, res) => {
+   res.sendFile('about.html', { root: __dirname });
+});
+
+    app.get('/contact-me', (req, res) => {
+  res.sendFile('contact-me.html', { root: __dirname });
+});
+
+ app.use((req, res) => {
+  res.sendFile(join(__dirname, 'contact-me.html')) });
+
+
+  
